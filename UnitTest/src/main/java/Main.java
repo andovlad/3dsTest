b90190a234505c7ab896d7731c4d89be4fc626d0
MALAMEN Ivan <Ivan.MALAMEN@3ds.com>
2017-08-28 15:53:44 +0200

    Sample app

---
+++ UnitTest/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String args[]) {
+        //To make code fail please uncomment next string
+        //PrintStream writer = new PrintStream( new File("randInts.txt"));
+        System.out.println ("Test app");
+    }
+}
---
+++ UnitTest/src/test/java/SampleTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void test() {
+
+        //Replace "test" with other string to make test fail
+        String result = "test";
+
+        assertEquals("test", result);
+
+    }
+}
