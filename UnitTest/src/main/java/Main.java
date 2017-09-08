a9bccee09175aff4f9029df4f2ee77ba0de7e136
hindp <Praveen.HindrolliSrinivasa@in.pega.com>
2017-09-08 17:18:38 +0530

    Changes to gradle files for build and test

--- UnitTest/src/main/java/Main.java
+++ UnitTest/src/main/java/Main.java
@@ -1,7 +1,20 @@
 public class Main {
+	
+	
+	public String printing(String text){
+        return text;
+    }
+	
+	public String printing2(){
+        return "test";
+    }
+	
     public static void main (String args[]) {
         //To make code fail please uncomment next string
         //PrintStream writer = new PrintStream( new File("randInts.txt"));
-        System.out.println ("Test app");
+			Main mn = new Main(); // create a new object of the class Study
+			String result = mn.printing("test"); 		
+			System.out.println (result);
+		
     }
 }
--- UnitTest/src/test/java/SampleTest.java
+++ UnitTest/src/test/java/SampleTest.java
@@ -2,14 +2,17 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
+
 public class SampleTest {
 
     @Test
     public void test() {
 
         //Replace "test" with other string to make test fail
-        String result = "test";
-
+        String result = "test2";
+		
+		Main myUnit = new Main();
+        result = myUnit.printing2();		
         assertEquals("test", result);
 
     }
